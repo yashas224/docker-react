7a65ceedd1f9d15fb283310f90699da9058b8003
yashas224 <71977329+yashas224@users.noreply.github.com>
2021-06-13 18:01:48 +0530

    update
    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yashas Samaga Krishnamorthy samagaEdit <code>src/App.js</code> and save to reload.
+          Yashas Samaga Krishnamorthy samaga Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
