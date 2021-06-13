13e3b9faaa8fd9e49d440017c69f4ca426deeb05
yashas224 <71977329+yashas224@users.noreply.github.com>
2021-06-13 19:37:46 +0530

    Update App.js
    change text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Yashas Samaga Production quality !!!Edit <code>src/App.js</code> and save to reload.
         </p>
+		
+		<p>
+			This was added on feature branch
+		</p>
         <a
           className="App-link"
           href="https://reactjs.org"
